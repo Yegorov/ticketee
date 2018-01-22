9b6ad0f17e93fa758e5ce06de7e567cdaa830d59
Yegorov <yegorov0725@yandex.ru>
2018-01-22 16:27:39 +0300

    Added permissions for editing tickets

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe TicketPolicy do
 
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for viewers of the project" do
@@ -28,6 +29,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for editors of the project" do
@@ -35,6 +37,13 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should_not permit_action :update }
+
+      context "when the editor created the ticket" do
+        before { ticket.author = user }
+
+        it { should permit_action :update }
+      end
     end
 
     context "for managers of the project" do
@@ -42,6 +51,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
 
     context "for managers of other projects" do
@@ -51,6 +61,7 @@ RSpec.describe TicketPolicy do
 
       it { should_not permit_action :show }
       it { should_not permit_action :create }
+      it { should_not permit_action :update }
     end
 
     context "for administrators" do
@@ -58,6 +69,7 @@ RSpec.describe TicketPolicy do
 
       it { should permit_action :show }
       it { should permit_action :create }
+      it { should permit_action :update }
     end
   end
 
